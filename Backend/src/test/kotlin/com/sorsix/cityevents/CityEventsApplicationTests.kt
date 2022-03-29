81381e715e12e9c2434211966112125fea2ec844
Dragan Bozhinoski <dragan.bozhinoski@students.finki.ukim.mk>
2022-03-29 13:54:07 +0200

    initial commit

---
+++ Backend/src/test/kotlin/com/sorsix/cityevents/CityEventsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sorsix.cityevents
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CityEventsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
