cc41469034487d7f8284c694c5811cb21c4b842e
Dragan Bozhinoski <dragan.bozhinoski@students.finki.ukim.mk>
2022-04-06 16:07:25 +0200

    minor fixes

--- Backend/src/main/kotlin/com/sorsix/cityevents/repository/TablesRepository.kt
+++ Backend/src/main/kotlin/com/sorsix/cityevents/repository/TablesRepository.kt
@@ -1,8 +1,13 @@
 package com.sorsix.cityevents.repository
 
+import com.sorsix.cityevents.domain.Reservation
 import com.sorsix.cityevents.domain.Table
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface TablesRepository:JpaRepository<Table,Long>
+interface TablesRepository:JpaRepository<Table,Long> {
+    @Query("update Table t set t.reservation=:reservation,t.reserved=:reserved where t.id=:id")
+    fun updateTable(id:Long,reservation:Reservation,reserved:Boolean)
+}
