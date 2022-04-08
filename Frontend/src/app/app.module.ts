02d9e60f47ce451925e15867715316b3cf37946c
jovankit <jovankitanov34@gmail.com>
2022-04-08 15:57:59 +0200

    locales and events page inint, home bage tags

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -8,13 +8,19 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { EventsHomeComponent } from './events-home/events-home.component';
+import { ListLocalesComponent } from './list-locales/list-locales.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListEventsComponent } from './list-events/list-events.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    EventsHomeComponent
+    EventsHomeComponent,
+    ListLocalesComponent,
+    FooterComponent,
+    ListEventsComponent
   ],
   imports: [
     BrowserModule,
---
+++ Frontend/src/app/list-events/events.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CityEvent } from 'src/model/CityEvent';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+
+  constructor(private http: HttpClient) { 
+  }
+
+  getEvents(): Observable<CityEvent[]> {
+    return this.http.get<CityEvent[]>(
+      "http://localhost:8082/api/events"
+    );
+  }
+}
