0005316eaf5c12428c51fc73d32fff72cde25858
Dragan Bozhinoski <72856858+draganbozhinoski@users.noreply.github.com>
2022-04-15 20:02:41 +0200

    add guard for security

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { EventFormComponent } from './event-form/event-form.component';
 import { EventPageComponent } from './event-page/event-page.component';
 import { HomeComponent } from './home/home.component';
@@ -10,9 +11,21 @@ import { LocalesFormComponent } from './locales-form/locales-form.component';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
-  { path: 'locales/create', component: LocalesFormComponent},
+  { 
+    path: 'locales/create', component: LocalesFormComponent,
+    canActivate: [AuthGuard],
+    data: {
+      role: 'ADMIN'
+    }
+  },
   { path: 'locales', component: ListLocalesComponent},
-  { path: 'events/create', component: EventFormComponent},
+  { 
+    path: 'events/create', component: EventFormComponent,
+    canActivate: [AuthGuard],
+    data: {
+      role: 'ADMIN'
+    }
+  },
   { path: 'events/:id', component: EventPageComponent},
   { path: 'events', component: ListEventsComponent},
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
