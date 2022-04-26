360d1105873dbe2306c9421402723af90348872e
jovankit <jovankitanov34@gmail.com>
2022-04-27 00:26:45 +0200

    front

---
+++ Frontend/src/app/reserve-form/reserve-form.component.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
+import { EventsService } from '../events.service';
+
+@Component({
+  selector: 'app-reserve-form',
+  templateUrl: './reserve-form.component.html',
+  styleUrls: ['./reserve-form.component.css']
+})
+export class ReserveFormComponent implements OnInit {
+  eventId:Number|undefined
+  reservation=new FormGroup({
+    date: new FormControl(null,Validators.required),
+    description: new FormControl(null,Validators.required)
+  })
+  
+
+  constructor(private route: ActivatedRoute,private service: EventsService,private http:HttpClient) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.pipe(
+      filter(params=>params.has("id")),
+      map(params=>+params.get("id")!)
+      ).subscribe(data=>this.eventId=data);
+  }
+
+  submit(){
+    this.service.reserve(
+      this.eventId!,
+      "jovie",
+      this.reservation.controls["date"].value,
+      "jovie",
+      435345,
+      this.reservation.controls["description"].value
+    ).subscribe()
+  }
+
+}
