f6a66657224e6c09ac3100502458d6b1647f672f
jovankit <jovankitanov34@gmail.com>
2022-05-01 22:03:11 +0200

    preFinal

--- Frontend/src/app/reserve-form/reserve-form.component.ts
+++ Frontend/src/app/reserve-form/reserve-form.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { filter, map, tap } from 'rxjs';
 import { EventsService } from '../events.service';
+import { TokenStorageService } from '../_services/token-storage.service';
 
 @Component({
   selector: 'app-reserve-form',
@@ -18,7 +19,7 @@ export class ReserveFormComponent implements OnInit {
   })
   
 
-  constructor(private route: ActivatedRoute,private router: Router,private service: EventsService,private http:HttpClient) { }
+  constructor(private route: ActivatedRoute,private router: Router,private service: EventsService,private http:HttpClient,private token:TokenStorageService) { }
 
   ngOnInit(): void {
     this.route.paramMap.pipe(
@@ -30,10 +31,10 @@ export class ReserveFormComponent implements OnInit {
   submit(){
     this.service.reserve(
       this.eventId!,
-      "jovie",
+      this.token.getUser().name,
       this.reservation.controls["date"].value,
-      "jovie",
-      435345,
+      this.token.getUser().username,
+      this.token.getUser().phoneNumber,
       this.reservation.controls["description"].value
     ).subscribe()
     this.router.navigateByUrl(`events/${this.eventId}`)
